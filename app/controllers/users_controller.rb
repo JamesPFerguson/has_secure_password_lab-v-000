1c8d000d9171e3382827c93941e27c860d8555a2
James Ferguson <ncjamesferguson@gmail.com>
2018-04-18 00:23:49 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params).save
+    @user = User.new(user_params)
+
+   if @user.save
+     session[:user_id] = @user.id
+     redirect_to home_path
+   else
+     flash[:alert] = "Passwords need to match"
+     redirect_to signup_path
+   end
   end
 
   def user_params
